e64e7030978388e6ac00c174da44893988a97b1e
Junzi Sun <junzisun@gmail.com>
2024-01-07 13:28:58 +0100

    null-ls

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,12 @@
 return {
-    "nvim-treesitter/nvim-treesitter", 
-    build = ":TSUpdate",
-    config = function()
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html", "css", "python" },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },  
-        })
-    end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
-
