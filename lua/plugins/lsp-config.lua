2f11b4dabf8a6b08d42a1f7a6585b8ec91a9c688
Junzi Sun <junzisun@gmail.com>
2024-01-07 17:25:43 +0100

    format

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,7 +16,7 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
 
 			lspconfig.lua_ls.setup({
